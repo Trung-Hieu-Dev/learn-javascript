994005b7a5f5fbc2e6627f4094228e378ded38e4
Trung_Hieu_Dev <dangtrunghieusg@gmail.com>
2022-12-28 14:21:34 +0700

    feat(add-function): Add fucntion to button

--- udemy-course/basic/basics-01-starting-project/assets/scripts/app.js
+++ udemy-course/basic/basics-01-starting-project/assets/scripts/app.js
@@ -1,23 +1,56 @@
 //define variable
-const defaultResult = 0; //assign value to variable
+const defaultResult = 10; //assign value to variable
 
 let currentResult = defaultResult;
 
 function getUserInput() {
-    return parseInt(usrInput.value)
+    return parseInt(usrInput.value);
+}
+
+function writeResultOutput(operator, enteredNumber) {
+    const calcDescription =  `${currentResult} ${operator} ${enteredNumber}`;
+
+    if (operator === '+') {
+        currentResult = currentResult + enteredNumber;
+    } else if (operator === '-') {
+        currentResult = currentResult - enteredNumber;
+    } else if (operator === '*') {
+        currentResult = currentResult * enteredNumber;
+    } if (operator === '/') {
+        currentResult = currentResult / enteredNumber;
+    }
+
+    outputResult(currentResult, calcDescription);
+    
 }
 
 //define fucntion
 function add() {
-
     const enteredNumber = getUserInput();
+    writeResultOutput('+', enteredNumber);
+}
 
-    const calcDescription =  `${currentResult} + ${enteredNumber}`;
-    currentResult = currentResult + enteredNumber;
+function subtract() {
+    const enteredNumber = getUserInput();
+    writeResultOutput('-', enteredNumber);
+}
 
-    outputResult(currentResult, calcDescription);
+function multiply() {
+    const enteredNumber = getUserInput();
+    writeResultOutput('*', enteredNumber);
+}
+
+function devide() {
+    const enteredNumber = getUserInput();
+    writeResultOutput('/', enteredNumber);
 }
 
 addBtn.addEventListener('click', add); //use add -> excute later when the button click
+
+subtractBtn.addEventListener('click', subtract);
+
+multiplyBtn.addEventListener('click', multiply);
+
+divideBtn.addEventListener('click', devide);
 
 
