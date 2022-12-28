022449b73b883b71a90f15864b2c375733fa6f31
Trung_Hieu_Dev <dangtrunghieusg@gmail.com>
2022-12-28 10:40:20 +0700

    fix(function): Refactor code

--- udemy-course/basic/basics-01-starting-project/assets/scripts/app.js
+++ udemy-course/basic/basics-01-starting-project/assets/scripts/app.js
@@ -3,10 +3,19 @@ const defaultResult = 0; //assign value to variable
 
 let currentResult = defaultResult;
 
+function getUserInput() {
+    return parseInt(usrInput.value)
+}
+
 //define fucntion
 function add() {
-    currentResult = currentResult + userInput.value;
-    outputResult(currentResult);
+
+    const enteredNumber = getUserInput();
+
+    const calcDescription =  `${currentResult} + ${enteredNumber}`;
+    currentResult = currentResult + enteredNumber;
+
+    outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener('click', add); //use add -> excute later when the button click
