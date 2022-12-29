5c743f16b568066cbd3da0160452372e55bfca9e
Trung_Hieu_Dev <dangtrunghieusg@gmail.com>
2022-12-29 09:30:22 +0700

    feat(monster-game): Set strong attack event

--- udemy-course/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ udemy-course/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,4 +1,6 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
+const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -6,11 +8,40 @@ let currentplayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-//attack
+//normal attack
 function attackHandler() {
     const damage = dealMonsterDamage(ATTACK_VALUE);
     currentMonsterHealth -= damage;
+
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentplayerHealth -= playerDamage;
+
+    if (currentMonsterHealth <= 0  && currentplayerHealth > 0) {
+        alert('You won !');
+    } else if(currentplayerHealth <= 0  && currentMonsterHealth > 0) {
+        alert('You lost !');
+    } else if(currentMonsterHealth <= 0 && currentplayerHealth <= 0) {
+        alert('You have a draw !')
+    }
+}
+
+//strong attack
+function strongAttackHandler() {
+    const damage = dealMonsterDamage(STRONG_ATTACK_VALUE);
+    currentMonsterHealth -= damage;
+
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentplayerHealth -= playerDamage;
+
+    if (currentMonsterHealth <= 0  && currentplayerHealth > 0) {
+        alert('You won !');
+    } else if(currentplayerHealth <= 0  && currentMonsterHealth > 0) {
+        alert('You lost !');
+    } else if(currentMonsterHealth <= 0 && currentplayerHealth <= 0) {
+        alert('You have a draw !')
+    }
 }
 
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler)
 
