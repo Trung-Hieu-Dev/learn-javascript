b9fe5609e5b8b95080b41fd2ce41c44fd14e7932
Trung_Hieu_Dev <dangtrunghieusg@gmail.com>
2023-01-02 17:58:46 +0700

    feat(dom): Mini project - Control Buttons

--- udemy-course/dom-mini-project/assets/scripts/app.js
+++ udemy-course/dom-mini-project/assets/scripts/app.js
@@ -1,8 +1,26 @@
 const addMovieModal = document.getElementById('add-modal');
 const startAddMovieBtn = document.querySelector('header button');
+const backdrop = document.getElementById('backdrop');
+const cancelMovieBtn = addMovieModal.querySelector('.btn--passive');
+
+
+const toggleBackdrop = () => {
+    backdrop.classList.toggle('visible');
+}
 
 const toggleMovieModal = () => {
     addMovieModal.classList.toggle('visible');
+    toggleBackdrop();
+}
+
+const backdropClickHandler = () => {
+    toggleMovieModal();
+}
+
+const cancelAddMovie = () => {
+    toggleMovieModal();
 }
 
 startAddMovieBtn.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', backdropClickHandler);
+cancelMovieBtn.addEventListener('click', cancelAddMovie);
