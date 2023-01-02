d7dd157d9d79e0a5d0c326c9f928e638de5c7892
Trung_Hieu_Dev <dangtrunghieusg@gmail.com>
2023-01-02 18:49:52 +0700

    feat(dom): Mini project - List Movies

--- udemy-course/dom-mini-project/assets/scripts/app.js
+++ udemy-course/dom-mini-project/assets/scripts/app.js
@@ -4,7 +4,33 @@ const backdrop = document.getElementById('backdrop');
 const cancelMovieBtn = addMovieModal.querySelector('.btn--passive');
 const confirmAddMovieBtn = cancelMovieBtn.nextElementSibling;
 const userInputs = addMovieModal.querySelectorAll('input');
+const entryTextSection = document.getElementById('entry-text');
 
+const movies= [];
+
+const updateUi = () => {
+    if(movies.length === 0) {
+        entryTextSection.style.display = 'block';
+    } else {
+        entryTextSection.style.display = 'none';
+    }
+}
+
+const renderNewMovieElement = (title, imageUrl, rating) => {
+    const newMovieElement = document.createElement('li');
+    newMovieElement.className = 'movie-element';
+    newMovieElement.innerHTML = `
+        <div class="movie-element__image">
+            <img src="${imageUrl}" alt="${title}">
+        </div>
+        <div class="movie-element__info">
+            <h2>${title}</h2>
+            <p>${rating} / 5 stars</p>
+        </div>
+    `
+    const listRoot = document.getElementById('movie-list');
+    listRoot.append(newMovieElement);
+}
 
 const toggleBackdrop = () => {
     backdrop.classList.toggle('visible');
@@ -21,6 +47,8 @@ const backdropClickHandler = () => {
 
 const cancelAddMovieHandler = () => {
     toggleMovieModal();
+    clearUserInput();
+
 }
 
 const addMovieHandler = () => {
@@ -36,6 +64,27 @@ const addMovieHandler = () => {
         +ratingValue > 5
     ) {
         alert('Please enter valid values (rating between 1 and 5).')
+    }
+
+    const newMovie = {
+        title: titleValue,
+        image: imageUrlValue,
+        rating: ratingValue
+    };
+    movies.push(newMovie);
+    console.log(movies);
+    toggleMovieModal();
+    clearUserInput();
+    renderNewMovieElement(newMovie.title, newMovie.image, newMovie.rating);
+    updateUi();
+}
+
+const clearUserInput = () => {
+    // userInputs[0].value = '',
+    // userInputs[1].value = '',
+    // userInputs[2].value = ''
+    for(const usrInput of userInputs) {
+        usrInput.value = '';
     }
 }
 
