2f5395507fb0061bc5d074571cedec9cf1872877
Trung_Hieu_Dev <dangtrunghieusg@gmail.com>
2023-01-02 18:14:17 +0700

    feat(demo): Mini project - Validate user inputs

--- udemy-course/dom-mini-project/assets/scripts/app.js
+++ udemy-course/dom-mini-project/assets/scripts/app.js
@@ -2,6 +2,8 @@ const addMovieModal = document.getElementById('add-modal');
 const startAddMovieBtn = document.querySelector('header button');
 const backdrop = document.getElementById('backdrop');
 const cancelMovieBtn = addMovieModal.querySelector('.btn--passive');
+const confirmAddMovieBtn = cancelMovieBtn.nextElementSibling;
+const userInputs = addMovieModal.querySelectorAll('input');
 
 
 const toggleBackdrop = () => {
@@ -17,10 +19,27 @@ const backdropClickHandler = () => {
     toggleMovieModal();
 }
 
-const cancelAddMovie = () => {
+const cancelAddMovieHandler = () => {
     toggleMovieModal();
+}
+
+const addMovieHandler = () => {
+    const titleValue = userInputs[0].value;
+    const imageUrlValue = userInputs[1].value;
+    const ratingValue = userInputs[2].value;
+
+    if(
+        titleValue.trim() === '' || 
+        imageUrlValue === '' || 
+        ratingValue === '' ||
+        +ratingValue < 1 || //add + to convert string to number
+        +ratingValue > 5
+    ) {
+        alert('Please enter valid values (rating between 1 and 5).')
+    }
 }
 
 startAddMovieBtn.addEventListener('click', toggleMovieModal);
 backdrop.addEventListener('click', backdropClickHandler);
-cancelMovieBtn.addEventListener('click', cancelAddMovie);
+cancelMovieBtn.addEventListener('click', cancelAddMovieHandler);
+confirmAddMovieBtn.addEventListener('click', addMovieHandler);
