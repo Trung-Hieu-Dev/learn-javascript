16d72ef8402b4968a715dd84c49460977c94a4a5
Trung_Hieu_Dev <dangtrunghieusg@gmail.com>
2023-01-05 18:04:59 +0700

    object: Destructuring Object

--- udemy-course/section09-objects/assets/scripts/objects.js
+++ udemy-course/section09-objects/assets/scripts/objects.js
@@ -5,67 +5,70 @@ const movies = [];
 
 //render list
 const renderMovies = (filter = '') => {
-    const moviesList = document.getElementById('movie-list');
+  const moviesList = document.getElementById('movie-list');
 
-    if (moviesList === 0) {
-        moviesList.classList.remove('visible');
-        return;
-    } else {
-        moviesList.classList.add('visible');
+  if (moviesList === 0) {
+    moviesList.classList.remove('visible');
+    return;
+  } else {
+    moviesList.classList.add('visible');
+  }
+
+  moviesList.innerHTML = '';
+
+  const filteredMovies = !filter
+    ? movies
+    : movies.filter((movie) => movie.info.title.includes(filter));
+
+  filteredMovies.forEach((movie) => {
+    const movieEl = document.createElement('li');
+
+    const { info } = movie;
+    const {title: movieTitle} = info; //const {title} = info; use this in case don't rename
+
+    let text = movieTitle + ' - ';
+    for (const key in info) {
+      if (key !== 'title') {
+        text = text + `${key}: ${movie.info[key]}`;
+      }
     }
-
-    moviesList.innerHTML = '';
-
-    const filteredMovies = !filter ? movies : movies.filter(movie => movie.info.title.includes(filter));
-
-    filteredMovies.forEach((movie) => {
-        const movieEl = document.createElement('li');
-        // movieEl.textContent = movie.info.title;
-        let text = movie.info.title + ' - ';
-        for (const key in movie.info) {
-            if (key !== 'title') {
-                text = text + `${key}: ${movie.info[key]}`;
-            }
-        }
-        movieEl.textContent = text;
-        moviesList.append(movieEl);
-    });
-}
+    movieEl.textContent = text;
+    moviesList.append(movieEl);
+  });
+};
 
 //add movie
 const addMovieHandler = () => {
-    const title = document.getElementById('title').value;
-    const extraName = document.getElementById('extra-name').value;
-    const extraValue = document.getElementById('extra-value').value;
+  const title = document.getElementById('title').value;
+  const extraName = document.getElementById('extra-name').value;
+  const extraValue = document.getElementById('extra-value').value;
 
-    if (
-        title.trim() === '' ||
-        extraName.trim() === '' ||
-        extraValue.trim() === ''
-    ) {
-        return;
-    }
+  if (
+    title.trim() === '' ||
+    extraName.trim() === '' ||
+    extraValue.trim() === ''
+  ) {
+    return;
+  }
 
-    const newMovie = {
-        info: {
-            title,
-            [extraName]: extraValue
-        },
-        id: Math.random()
-    }
+  const newMovie = {
+    info: {
+      title,
+      [extraName]: extraValue,
+    },
+    id: Math.random(),
+  };
 
-    movies.push(newMovie);
+  movies.push(newMovie);
 
-    renderMovies();
-
-    console.log(movies);
-}
+  renderMovies();
+};
 
 //search movie
 const searchMovieHandler = () => {
-    const filterTerm = document.getElementById('filter-title').value;
-    renderMovies(filterTerm);
-}
+  const filterTerm = document.getElementById('filter-title').value;
+  renderMovies(filterTerm);
+};
 
 addMovieBtn.addEventListener('click', addMovieHandler);
 searchBtn.addEventListener('click', searchMovieHandler);
